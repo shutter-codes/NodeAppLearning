416d6561aab7bf36c25133726e8636db4f4bea43
Naitik Sharma <snaitik0238@gmail.com>
2023-03-05 19:19:01 +0530

    added fs modules

---
+++ fsmodule/index.js
@@ -0,0 +1,19 @@
+// importing the module
+const fs = require('fs')
+
+
+//this is synchronous function without callback functionalities
+const file = fs.readFileSync('demo.txt','utf-8')
+console.log(file);
+  
+// this is asynchronous method with the call back functionalities
+fs.readFile('demo.txt', 'utf-8', (err,text) => {
+    console.log(text);
+
+})
+
+console.log("hello world");
+
+// in synchronous function , it will block the output for further instructions while asynchronous will not block the other process.
+
+// synchronous will take more time than asynchronous.
